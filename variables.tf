3fb12d90a5d4f0844568f82709b3ec51a7b8ffae
Akhil <akhilmahendra035@gmail.com>
2024-11-29 15:53:57 +0530

    Remove unwanted machines.

--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type        = "t3.small"
-      policy-resource-list = ["ec2:DescribeInstances"]
-    }
-
-    grafana = {
-      instance_type        = "t3.small"
-      policy-resource-list = []
-    }
+#     prometheus = {
+#       instance_type        = "t3.small"
+#       policy-resource-list = ["ec2:DescribeInstances"]
+#     }
+#
+#     grafana = {
+#       instance_type        = "t3.small"
+#       policy-resource-list = []
+#     }
 
     vault = {
       instance_type        = "t3.small"
       policy-resource-list = []
     }
 
-    elk = {
-      instance_type        = "r7i.large"
-      policy-resource-list = []
-    }
+#     elk = {
+#       instance_type        = "r7i.large"
+#       policy-resource-list = []
+#     }
 
     ci-server = {
       instance_type = "t3.small"
       policy-resource-list = []
     }
 
-    sonarqube = {
-      instance_type        = "t3.large"
-      policy-resource-list = []
-      }
-
-    artifactory = {
-      instance_type        = "t3.large"
-      policy-resource-list = []
-    }
+#     sonarqube = {
+#       instance_type        = "t3.large"
+#       policy-resource-list = []
+#       }
+#
+#     artifactory = {
+#       instance_type        = "t3.large"
+#       policy-resource-list = []
+#     }
 
     jenkins = {
       instance_type        = "t3.small"
