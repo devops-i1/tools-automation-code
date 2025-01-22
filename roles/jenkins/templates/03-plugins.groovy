8db973801446433d32de12ed5180d7db2e4dc952
Akhil <akhilmahendra035@gmail.com>
2025-01-22 12:51:44 +0530

    Adding ssh plugin

--- roles/jenkins/templates/03-plugins.groovy
+++ roles/jenkins/templates/03-plugins.groovy
@@ -13,6 +13,7 @@ final List<String> REQUIRED_PLUGINS = [
         "workflow-aggregator",
         "ws-cleanup",
         "blueocean",
+        "ssh-slaves",
         "multibranch-scan-webhook-trigger"
 ]
 
